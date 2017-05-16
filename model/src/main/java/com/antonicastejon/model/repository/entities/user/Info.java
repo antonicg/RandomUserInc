96312a6034faf5b5dbdafa92ad7b5b629d2bd2ed
Antoni Castejón <antonicg@gmail.com>
2017-05-16 21:23:13 +0200

    Added User related classes

---
+++ model/src/main/java/com/antonicastejon/model/repository/entities/user/Info.java
@@ -0,0 +1,43 @@
+
+package com.antonicastejon.model.repository.entities.user;
+
+public class Info {
+
+    private String seed;
+    private Long results;
+    private Long page;
+    private String version;
+
+    public String getSeed() {
+        return seed;
+    }
+
+    public void setSeed(String seed) {
+        this.seed = seed;
+    }
+
+    public Long getResults() {
+        return results;
+    }
+
+    public void setResults(Long results) {
+        this.results = results;
+    }
+
+    public Long getPage() {
+        return page;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+}
